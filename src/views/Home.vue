a6c05e9bef90b39fe70737256005767f4139fe47
nicotina04 <devherdson@gmail.com>
2019-10-22 01:38:00 +0900

    Implement App.vue can load mockup data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,30 @@
 <template>
-    <p>TESTING</p>
+    <div>
+        <div class="upperbox">
+            <p>새로운 경험을 할 장소를 찾아보세요!</p>
+        </div>
+        <div v-for="item in moc" :key="item.name">{{ item.name }}</div>
+    </div>
 </template>
 
 <script>
+import mockup from '../assets/mockup.js'
 
 export default {
     name: 'home',
     components: {
+    },
+    data () {
+        return {
+            moc: mockup
+        }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .upperbox {
+        height: 30%;
+        background-color: #F56C6C;
+    }
 </style>
