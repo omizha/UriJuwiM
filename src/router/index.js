cd3f4222f76eae1217be46908b3fc14fe469f25f
nicotina04 <devherdson@gmail.com>
2019-10-21 23:05:19 +0900

    Added 3 temp routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +7,22 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: Home
+        component: () => import('../views/Home.vue')
     },
     {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        path: '/home',
+        name: 'home',
+        component: () => import('../views/About.vue')
+    },
+    {
+        path: '/search',
+        name: 'search',
+        component: () => import('../views/About.vue')
+    },
+    {
+        path: '/map_view',
+        name: 'search',
+        component: () => import('../views/About.vue')
     }
 ]
 
