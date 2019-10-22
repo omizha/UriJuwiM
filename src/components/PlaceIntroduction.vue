dce96a233da6cc921c2e8682acdfae6d585d96f9
nicotina04 <devherdson@gmail.com>
2019-10-22 13:19:06 +0900

    Implement load introducing place using transition

---
+++ src/components/PlaceIntroduction.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>TESTING</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlaceIntroduction.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,24 +3,38 @@
         <div class="upperbox">
             <p>새로운 경험을 할 장소를 찾아보세요!</p>
         </div>
-        <div v-for="item in moc" :key="item.name">
-            <img :src="item.thumbnail">
-            <p>{{ item.name }}</p>
-        </div>
+        <transition name="slide-fade" :duration="200" mode="out-in">
+            <div id="places" v-if="!placeload">
+                <div v-for="item in moc" :key="item.name">
+                    <img :src="item.thumbnail">
+                    <p>{{ item.name }}</p>
+                </div>
+            </div>
+            <intro v-if='placeload'></intro>
+        </transition>
+<!--        <transition name="fade" mode="out-in">-->
+<!--            -->
+<!--        </transition>-->
+        <el-button @click="placeload = !placeload">TEST IT!</el-button>
     </div>
 </template>
 
 <script>
 import mockup from '../assets/mockup.js'
+import intro from '@/components/PlaceIntroduction.vue'
 
 export default {
     name: 'home',
     components: {
+        intro
     },
     data () {
         return {
-            moc: mockup
+            moc: mockup,
+            placeload: false
         }
+    },
+    methods: {
     }
 }
 </script>
