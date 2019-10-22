5522f5c830db0f36a1ebd84f23e3f253688dff25
nicotina04 <devherdson@gmail.com>
2019-10-22 20:56:51 +0900

    PlaceIntroduction.vue can show location info

--- src/components/PlaceIntroduction.vue
+++ src/components/PlaceIntroduction.vue
@@ -1,15 +1,37 @@
 <template>
     <div>
-        <p>TESTING</p>
+        <div class="wrapper">
+            <div class="imgbox">
+                <el-image :src="info.url"></el-image>
+            </div>
+            <div class="spotloaction">
+                <p>{{ info.Location }}</p>
+                <p>{{ info.Address }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    props: ['info'],
     name: 'PlaceIntroduction.vue'
 }
 </script>
 
 <style scoped>
-
+.wrapper {
+    margin: auto;
+}
+.imgbox {
+    max-width: 100%;
+    max-height: 4%;
+    margin: auto;
+}
+.spotloaction {
+    text-align: left;
+    color: #FFFFFF;
+    font-size: 20px;
+    background: #F56C6C;
+}
 </style>
