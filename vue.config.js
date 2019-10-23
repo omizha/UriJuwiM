d358a538c3139e0ede16a406ad5318a3ed67fc1d
maemesoft <maemesoft@naver.com>
2019-10-23 10:25:15 +0900

    Fix devServer

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    devServer: {
+        host: '0.0.0.0',
+        allowedHosts: ['maemesoft.iptime.org']
+    },
     chainWebpack: (config) => {
         const svgRule = config.module.rule('svg')
 
