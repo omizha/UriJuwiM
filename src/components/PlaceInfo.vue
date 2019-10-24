b8f690e80fcc5ff9a89330884500d124073c13d8
nicotina04 <devherdson@gmail.com>
2019-10-24 10:34:30 +0900

    [PlaceInfo.vue] Initialize component

---
+++ src/components/PlaceInfo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="alertbox">
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .alertbox {
+        width: 500px;
+        height: 240px;
+    }
+</style>
