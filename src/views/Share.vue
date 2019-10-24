dfd52313f26d5b9981a9308b9d0c78b609bce753
nicotina04 <devherdson@gmail.com>
2019-10-25 03:38:02 +0900

    Share.vue (Not complete)

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -3,9 +3,13 @@
         <div v-if="anone === true">
             <div id="firebaseui-auth-container"></div>
         </div>
-        <div v-else>
-            <h1>{{ this.displayName }} 님! 반갑습니다!</h1>
-                <div class="cameraInfoView" v-show="showit">
+        <div v-else style="margin-bottom: 60px">
+            <div>
+                <el-button v-on:click='Logout' style="float: left">로그아웃</el-button>
+                <h1>{{ this.displayName }} 님! 반가워요!</h1>
+            </div>
+            <el-button round style="background: #def5b8; color: #FFFFFF" v-on:click="rotate" v-show="!showit">글쓰기</el-button>
+                <div class="cameraInfoView" v-show="!showit">
                     <div v-for="item in this.datas" :key="item.url">
                         <el-card class="box-card">
                             <div slot="header" class="clearfix">
@@ -17,12 +21,25 @@
                             <p style="text-align: left">{{ item.Content }}</p>
                         </el-card>
                     </div>
-                    <el-button round style="background: #def5b8; color: #FFFFFF" v-on:click="writeSomething">글쓰기</el-button>
                 </div>
-            <el-button v-on:click='Logout'>로그아웃</el-button>
         </div>
-        <div v-show="!showit">
-            <el-form class="former"></el-form>
+        <div v-show="showit" style="overflow-y: auto">
+            <p style="text-align: left">내용</p>
+            <el-input type="textarea" class="former" v-model="text"></el-input>
+            <el-divider></el-divider>
+            <p style="text-align: left">위치</p>
+            <el-input v-model="location"></el-input>
+            <p style="text-align: left">장소</p>
+            <el-input v-model="area"></el-input>
+            <p style="text-align: left">사진 업로드</p>
+            <input type="file" @change="previewImage" accept="image/*">
+            <div class="image-preview" v-if="this.imgbase.length > 0">
+                <img class="preview" :src="this.imgbase">
+            </div>
+            <div style="margin-bottom: 75px">
+                <el-button @click="writeSomething">저장</el-button>
+                <el-button @click="rotate">취소</el-button>
+            </div>
         </div>
     </div>
 </template>
@@ -30,6 +47,7 @@
 <script>
 import * as firebaseui from 'firebaseui'
 import * as firebase from 'firebase/app'
+import 'firebase/storage'
 import 'firebase/firestore'
 import config from '@/assets/fireconfig.js'
 
@@ -46,7 +64,11 @@ export default {
             displayName: '',
             email: '',
             datas: [],
-            showit: false
+            showit: false,
+            text: '',
+            location: '',
+            area: '',
+            imgbase: ''
         }
     },
     methods: {
@@ -82,10 +104,10 @@ export default {
                                 .then((snap) => {
                                     snap.forEach((docu) => {
                                         this.datas.push(docu.data())
+                                        console.log(this.datas)
                                     })
                                 })
                         })
-                        this.showit = true
                     })
                 this.anone = false
             } else {
@@ -106,7 +128,56 @@ export default {
             })
         },
         writeSomething: function () {
-            this.showit = true
+            if (!this.text) {
+                alert('내용을 작성하십시오.')
+                return
+            }
+
+            var storageRef = firebase.storage().ref()
+            let imagename = this.imgbase.substring(23, 40) + '.jpg'
+            let addchild = storageRef.child(imagename)
+
+            if (this.imgbase) {
+                addchild.putString(this.imgbase, 'data_url').then((snap) => {
+                    addchild.getDownloadURL().then((urli) => {
+                        db.collection('data').doc(this.uid).collection('timeline').add({
+                            Address: '경기도 봉황시 천리마마트',
+                            CameraModel: 'Phone',
+                            CaptureDate: new Date().toLocaleTimeString(),
+                            Content: this.text,
+                            Keyword: '야옹, 왈왈, 그르렁',
+                            Location: this.location,
+                            PhotoLocation: this.area,
+                            url: urli
+                        }).then(() => {
+                            this.rotate()
+                        })
+                    })
+                })
+            } else {
+
+            }
+        },
+        rotate: function () {
+            this.showit = !this.showit
+        },
+        previewImage: function (event) {
+            // Reference to the DOM input element
+            var input = event.target
+            // Ensure that you have a file before attempting to read it
+            if (input.files && input.files[0]) {
+                // create a new FileReader to read this image and convert to base64 format
+                var reader = new FileReader()
+                // Define a callback function to run, when FileReader finishes its job
+                reader.onload = (e) => {
+                    // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
+                    // Read image as base64 and set to imageData
+                    this.imgbase = e.target.result
+                    // console.log(this.imgbase)
+                }
+                // Start the reader job - read file as a data url (base64 format)
+                reader.readAsDataURL(input.files[0])
+            }
         }
     },
     computed: {
@@ -136,9 +207,10 @@ export default {
     .cameraInfoView {
         padding : 16px;
     }
-
     .former {
-        min-height: 100px;
         width: 88%;
+    }
+    .preview {
+        max-width: 300px;
     }
 </style>
